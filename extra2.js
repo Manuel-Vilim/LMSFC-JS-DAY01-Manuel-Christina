d324b714bb44c6e1d65f50222ba9ddeb44deca17
Christina Renieris <christinarenieris@gmail.com>
2020-01-23 15:31:17 +0100

    extra

---
+++ extra2.js
@@ -0,0 +1,8 @@
+/*fruit = ['apple', 'banana', 'kiwi'];
+console.log(fruit.slice(0,2),'orange','strawberry',fruit.slice(2,3));
+*/
+
+
+fruit = ['apple', 'banana', 'kiwi'];
+fruit.splice (2,0 ,'orange','strawberry');
+console.log(fruit);
