d0994e29bfb082daf5c63d57baedde51a14dcd57
Manuel Vilim <Manuel.Vilim@gmx.at>
2020-01-23 14:06:12 +0100

    Challenge

---
+++ Challenge.js
@@ -0,0 +1,14 @@
+var br = "<br>";
+
+var city = 'Vienna is a nice city';
+var temp = new Array();
+temp [0]='Vienna ';
+temp [1]='is ';
+temp [2]='a ';
+temp [3]='nice ';
+temp [4]='city ';
+document.write(temp[0]);
+document.write(temp[1]);
+document.write(temp[2]);
+document.write(temp[3]);
+document.write(temp[4]);
